6c90eb2aa1b0b288d475d09bf26629f5f0cac070
The Fallen One <djdj83059@gmail.com>
2024-12-08 19:47:36 +0500

    Chsnges successful

--- Learning.cpp
+++ Learning.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using std::cout;
+using std::cin;
+using std::endl;
 
 int main(){
 
 cout<<"Hello World";
+
+int musfira_age;
+cout<<"Enter the age"<<endl;
+cin>>musfira_age;
+if(musfira_age<=18){
+    cout<<"Chutku"<<endl;
+}
+else{
+    cout<<"Musfira budhi"<<endl;
+}
+
+return 0;
 
 }
